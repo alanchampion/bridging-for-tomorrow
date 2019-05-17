67cba4cd154eebd96aab47b2f9ef4bd5fce309bf
Alan Champion <alan.champion@drillinginfo.com>
2019-05-17 12:52:02 -0600

    Testing file reading

--- src/main/java/com/meanymellow/bridgingfortomorrow/controller/DataManipulationController.java
+++ src/main/java/com/meanymellow/bridgingfortomorrow/controller/DataManipulationController.java
@@ -32,12 +32,16 @@ public class DataManipulationController {
         storageService.loadAll().forEach(path -> {
             BufferedReader br = null;
             try {
+                String current = new java.io.File( "." ).getCanonicalPath();
+                System.out.println("Current dir: "+current);
+                String currentDir = System.getProperty("user.dir");
+                System.out.println("Current dir using System: " +currentDir);
                 System.out.println("Get File Name: " + path.getFileName().toString());
                 System.out.println("To File Absolute: " + path.toFile().getAbsolutePath());
                 System.out.println("To File Canonical: " + path.toFile().getCanonicalPath());
                 System.out.println("To File Path: " + path.toFile().getPath());
                 System.out.println("To File Name: " + path.toFile().getName());
-                br = new BufferedReader(new FileReader(path.toFile()));
+                br = new BufferedReader(new FileReader(storageService.loadAsResource(path.getFileName().toString()).getFile()));
                 System.out.println(br.readLine());
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
